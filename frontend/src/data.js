b2247bb04a08b98e0e142d90c26230fb1f6d5d68
TonyB <brigettony@gmail.com>
2020-11-21 08:07:41 +0100

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,39 @@
+const data = {
+    products:[
+        {
+            _id:"1",
+            name:"Adidas Slim Shirt",
+            category:"Shirts",
+            image:"/images/p1.jpg",
+            price: 120,
+            brand:"Nike",
+            rating: 4.8,
+            numReviews:10,
+            description: "High quality product"
+        },
+        {
+            _id:"2",
+            name:"Nike Pants",
+            category:"Shirts",
+            image:"/images/p1.jpg",
+            price: 80,
+            brand:"Nike",
+            rating: 4.5,
+            numReviews:10,
+            description: "High quality product"
+        },
+        {
+            _id:"3",
+            name:"Puma Shoes",
+            category:"Shirts",
+            image:"/images/p1.jpg",
+            price: 56,
+            brand:"Nike",
+            rating: 4,
+            numReviews:10,
+            description: "High quality product"
+        }
+    ]
+}
+
+export default data;
